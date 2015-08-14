329416f6fbbf00b8c10c52a1178a580bff366920
a1320521 <a1320521@192.168.1.70>
2015-08-14 19:27:17 -0300

    Criado classe de conecção com o banco de dados

---
+++ Campeonato/src/campeonato/Campeonato.java
@@ -0,0 +1,10 @@
+
+package campeonato;
+
+public class Campeonato {
+
+    public static void main(String[] args) {
+       
+    }
+    
+}
---
+++ Campeonato/src/campeonato/ConnectFactori.java
@@ -0,0 +1,30 @@
+
+
+package Campeonato;
+
+import java.sql.*;
+import javax.swing.JOptionPane;
+
+public class ConnectFactori{
+    
+    public static Connection getPostgresSQLConnection(){
+        Connection con = null;
+        
+        try{
+            Class.forName("org.postgresql.Driver");      
+            
+            con = DriverManager.getConnection("jdbc:postgresql://localhost:5432/campeonatoIFSP","postgres","0");
+        
+        }
+        catch(Exception Ex){
+            
+            System.out.println("Não estabelecido coneção");
+        };
+        
+        return con;
+    }
+    
+    
+    
+}
+
